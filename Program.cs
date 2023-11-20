e74c84f956dcf136276b8a6ee7d636b53eac8385
Milada kasparova <miladakas@gmail.com>
2023-11-20 10:42:22 +0100

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int userInput1 = 12;
+int userInput2 = 25;
+int userInput3 = 3; 
+for (int i = 0; i < userInput2; i += userInput3)
+{
+    Console.WriteLine(i);
+}
+{ int i = 0;
+   // i + 1;
+    i = i + 1;
+    i += 1;
+}
+
+string userInput4 = Console.ReadLine();
+int userInput5 = 20;
+for (int i = 0; i < userInput5; i++)
+{
+    Console.WriteLine($"{i}. {userInput4}");
+}
